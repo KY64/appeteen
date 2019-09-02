aca4a90a04bc12a335172ea15036ba4be0bfc86f
KY64 <taurus99@protonmail.com>
2019-09-02 23:37:22 +0700

    no change yet

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-const withOffline = require("next-offline");
+// const withOffline = require("next-offline");
 
-const nextConfig = {
-  workboxOpts: {
-    globPatterns: ["static/**/*"],
-    globDirectory: ".",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200
-          }
-        }
-      }
-    ]
-  }
-};
+// const nextConfig = {
+//   workboxOpts: {
+//     globPatterns: ["static/**/*"],
+//     globDirectory: ".",
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: "NetworkFirst",
+//         options: {
+//           cacheName: "offlineCache",
+//           expiration: {
+//             maxEntries: 200
+//           }
+//         }
+//       }
+//     ]
+//   }
+// };
 
-module.exports = withOffline(nextConfig);
+// module.exports = withOffline(nextConfig);
